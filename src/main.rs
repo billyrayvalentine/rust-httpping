c34539faf85b433ab33f8dda297c7cac913bcd7f
= <electronicbb@mykolab.com>
2024-12-13 16:58:10 +0000

    chore: WIP

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 // Cli app entry point
 use clap::Parser;
+use std::process;
+use ureq::{Error, OrAnyStatus, Response};
+use url::Url;
+//use std::fmt::Error;
 #[derive(Parser, Debug)]
 #[command(version, about, author)]
 
@@ -9,14 +13,43 @@ struct Args {
     destination: String,
     /// Stop after <count> replies
     #[arg(short, long, value_parser = clap::value_parser!(u64).range(1..))]
-    count: u64,
+    count: Option<u64>,
     /// Follow redirects
     #[arg(short, long)]
     redirects: bool,
 }
 
-fn main() {
+// test destination is parsable by Url::parse() and is http(s)
+fn validate_url_string_error(url: &str) -> Result<(), String> {
+    let parsed_url = Url::parse(url).map_err(|e| e.to_string())?;
+
+    println!("Got scheme {}", parsed_url.scheme());
+    //if parsed_url.scheme() != "http" || parsed_url.scheme() != "https" {
+    if parsed_url.scheme() != "https" {
+        return Err("Scheme must be http or https".to_string());
+    }
+
+    Ok(())
+}
+
+fn do_ping(url: &str) -> Result<ureq::Response, ureq::Error> {
+    let resp = ureq::request("HEAD", &url).call().or_any_status()?;
+    println!("Response was: {:?}", resp);
+    Ok(resp)
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
-    //println!("Got the filename {}", args.file);
-    dbg!(args);
+    dbg!(&args);
+
+    let url = validate_url_string_error(&args.destination).unwrap_or_else(|error| {
+        eprintln!("Error parsing destination: {error:?}");
+        process::exit(1);
+    });
+
+    if let Err(e) = do_ping(&args.destination) {
+        eprintln!("Got error {}", e);
+        process::exit(1)
+    }
+    Ok(())
 }
