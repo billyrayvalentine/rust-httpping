f5b99d539db780bbb1ded16ae7f64903be606404
BillyRay Valentine <electronicbb@mykolab.com>
2024-12-14 18:38:37 +0000

    feat: Absolute MVP

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
+//#![allow(unused_imports, unused_variables)]
+
 // Cli app entry point
 use clap::Parser;
+use std::process;
+use ureq::OrAnyStatus;
+use url::Url;
 #[derive(Parser, Debug)]
 #[command(version, about, author)]
 
@@ -9,14 +14,60 @@ struct Args {
     destination: String,
     /// Stop after <count> replies
     #[arg(short, long, value_parser = clap::value_parser!(u64).range(1..))]
-    count: u64,
+    count: Option<u64>,
     /// Follow redirects
     #[arg(short, long)]
     redirects: bool,
 }
 
+// test destination is parsable by Url::parse() and is http(s)
+fn validate_url_string_error(url: &str) -> Result<(), String> {
+    let parsed_url = Url::parse(url).map_err(|e| e.to_string())?;
+
+    //println!("Got scheme {}", parsed_url.scheme());
+    match parsed_url.scheme() {
+        "http" | "https" => Ok(()),
+        _ => Err("Scheme must be http or https".to_string()),
+    }
+}
+
+fn do_ping(url: &str) -> Result<(u16, String), ureq::Error> {
+    let resp = ureq::request("HEAD", url).call().or_any_status()?;
+    //dbg!(&resp);
+    let resp_output = (resp.status(), resp.get_url().to_string());
+    //dbg!(&resp_output);
+    Ok(resp_output)
+}
+
 fn main() {
     let args = Args::parse();
-    //println!("Got the filename {}", args.file);
-    dbg!(args);
+    //dbg!(&args);
+    let ping_forever = args.count.is_none();
+
+    validate_url_string_error(&args.destination).unwrap_or_else(|error| {
+        eprintln!("Error parsing destination: {error:?}");
+        process::exit(1);
+    });
+
+    let mut keep_pinging = true;
+    let mut ping_counter = 1;
+
+    while keep_pinging {
+        match do_ping(&args.destination) {
+            Err(e) => {
+                eprintln!("Got error {}", e);
+                process::exit(1);
+            }
+            Ok(resp) => {
+                println!("{} from {} seq={}", resp.0, resp.1, ping_counter);
+            }
+        }
+
+        if !ping_forever && ping_counter == args.count.unwrap() {
+            keep_pinging = false;
+        } else {
+            ping_counter += 1;
+        }
+    }
+    process::exit(0);
 }
---
+++ tests/cli.rs
@@ -0,0 +1,40 @@
+use assert_cmd::prelude::*;
+use predicates::prelude::*;
+use std::process::Command;
+
+#[test]
+fn test_args_none() {
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    cmd.assert().failure().code(2);
+}
+
+#[test]
+fn test_args_destination_garbage() {
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    cmd.arg("httpbin.org")
+        .assert()
+        .failure()
+        .code(1)
+        .stderr(predicate::str::starts_with("Error parsing destination:"));
+}
+
+#[test]
+fn test_args_destination_not_http_or_https() {
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    cmd.arg("ftp://httpbin.org")
+        .assert()
+        .failure()
+        .code(1)
+        .stderr(predicate::str::starts_with(
+            "Error parsing destination: \"Scheme must be http or https\"",
+        ));
+}
+
+#[test]
+fn test_args_destination_valid() {
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    cmd.arg("http://httpbin.org").arg("-c1").assert().success();
+
+    let mut cmd = Command::cargo_bin(env!("CARGO_PKG_NAME")).unwrap();
+    cmd.arg("https://httpbin.org").arg("-c1").assert().success();
+}
